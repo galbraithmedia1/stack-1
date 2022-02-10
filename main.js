61a1c2ee2eac02b4cd85d2db27674e2d6bd88158
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-10 16:42:15 -0700

    update

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
 const temp = document.querySelector(".temp");
-const body = document.querySelector("body");
-const button = document.querySelector(".button");
+const Key = "ed088ac32004347c3513041d87c48e51";
 
-
-
-
+//Below not needed unlesss running off a server
+// require('dotenv').config()
+// const API_KEY = process.env.API_KEY;
 
 const test = () => {
-    console.log("test");
-  axios.get("https://api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51").then((res) => {
-    let weather = res.data.weather[0].main;
-    // let weather = 20
-    console.log(weather);
-    
-    let text = document.createTextNode(weather)
-    temp.appendChild(text)
+  console.log("test");
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=${Key}`
+    )
+    .then((res) => {
+      let weather = res.data.weather[0].main;
+      console.log(weather);
 
-
-
-
-  });
+      let text = document.createTextNode(weather);
+      temp.appendChild(text);
+    });
 };
-
-test()
+test();
 
 // temp.addEventListener("load", test);
 // button.addEventListener("click", test)
-
-
-
-
-
-
-// const API_KEY = process.env.API_KEY
 
 // API link specific for Lehi Utah using Lon & Lat:
 // This key is also stored in the .env file. Not sure if the const API_KEY is the best way to link it over.
