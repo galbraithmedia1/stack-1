b4426ccbce0fe2d197d7c344e1fdf51c2777e86e
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-10 10:16:10 -0700

    Key update in .env

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+
 const temp = document.querySelector('temp')
+const API_KEY = process.env.API_KEY
 
 // API link specific for Lehi Utah using Lon & Lat: 
-// api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51
-
+// This key is also stored in the .env file. Not sure if the const API_KEY is the best way to link it over. 
 //API_Key = ed088ac32004347c3513041d87c48e51
 
-axios.get('api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51').then(res =>{
+// api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51
+
+
+
+axios.get('api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid={API_KEY}').then(res =>{
 
     console.log(res.data)    
     //let weather = res.data.weather
