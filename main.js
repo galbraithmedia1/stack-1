93441fd6c2639666421e45d15ab7e388f2a9c1d7
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-09 23:07:22 -0700

    api working

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const temp = document.querySelector('temp')
 
 //key or 3b04a5f822a9e727a4a0a5847fa0f167
-axios.get('api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ed088ac32004347c3513041d87c48e51').then(res =>{
+axios.get('api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51').then(res =>{
 
+    
    // let weather = res.data.weather
     let weather = 20
 
