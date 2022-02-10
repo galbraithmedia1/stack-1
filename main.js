a7bcaba21d7e6fc6c854ce59cb999869fea6680a
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-10 15:48:56 -0700

    update

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
+const temp = document.querySelector(".temp");
+const body = document.querySelector("body");
+const button = document.querySelector(".button");
 
-const temp = document.querySelector('temp')
-const API_KEY = process.env.API_KEY
 
-// API link specific for Lehi Utah using Lon & Lat: 
-// This key is also stored in the .env file. Not sure if the const API_KEY is the best way to link it over. 
+
+
+
+const test = () => {
+    console.log("test");
+  axios.get("https://swapi.dev/api/people/3").then((res) => {
+    let weather = res.data.name;
+    // let weather = 20
+    console.log(weather);
+    
+    let text = document.createTextNode(weather)
+    temp.appendChild(text)
+
+
+
+
+  });
+};
+
+temp.addEventListener("load", test);
+button.addEventListener("click", test)
+
+
+
+
+
+
+// const API_KEY = process.env.API_KEY
+
+// API link specific for Lehi Utah using Lon & Lat:
+// This key is also stored in the .env file. Not sure if the const API_KEY is the best way to link it over.
 //API_Key = ed088ac32004347c3513041d87c48e51
 
 // api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid=ed088ac32004347c3513041d87c48e51
-
-
-
-axios.get('api.openweathermap.org/data/2.5/weather?lat=40.39&lon=-111.85&appid={API_KEY}').then(res =>{
-
-    console.log(res.data)    
-    //let weather = res.data.weather
-    // let weather = 20
-
-
-   // temp.appendChild(weather)
-})
-
